2555aa04f9720c2ea49068e7feb3658701043f45
OXplakhoa <OXplakhoa@gmail.com>
2024-11-08 12:04:41 +0700

    Add Styles

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,8 +2,20 @@ import { Stack } from "expo-router";
 
 export default function RootLayout() {
   return (
+    <>  
     <Stack>
-      <Stack.Screen name="index" />
+      <Stack.Screen 
+      name="index"
+      options={{
+        headerShown: false,
+        // title: "Home",
+        // headerStyle: { backgroundColor: "#25293e" }, 
+        // headerTintColor: "white", 
+        // headerTitleStyle: { fontSize: 24 }, 
+        // contentStyle: { backgroundColor: "#ffffff" },
+      }}
+      />
     </Stack>
+    </>
   );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,19 +1,50 @@
+
 import { StyleSheet, Text, View } from "react-native";
-import { rgbaColor } from "react-native-reanimated/lib/typescript/reanimated2/Colors";
 
 export default function Index() {
   return (
-    <View
-      style={styles.container}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <View style={styles.city}>
+        <Text style={styles.cityName}>Ho Chi Minh</Text>
+      </View>
+      <View style={styles.weather}>
+        <View style={styles.day}>
+         <Text style={styles.temp}> 27 </Text>
+         <Text style={styles.description}> Sunny </Text>
+        </View>
+      </View>                                                                                     
     </View>
   );
 }
 
-const styles = StyleSheet.create({
+export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "crimson",
+    backgroundColor: "#ff4a2b",
+  },
+  city: {
+    flex: 1.2,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  cityName: {
+    fontSize: 68,
+    fontWeight: "500",
+  }
+  ,
+  weather: {
+    flex: 3,
+  },
+  day: {
+    flex: 1,
+    alignItems: "center",
+  },
+  temp: {
+    marginTop: 50,
+    fontSize: 178
+  },
+  description: {
+    marginTop: -30,
+    fontSize: 60,
   }
 })
