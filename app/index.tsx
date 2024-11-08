d215fef0cb387adda6b5bda504caf466b6e8b635
OXplakhoa <OXplakhoa@gmail.com>
2024-11-08 15:26:12 +0700

    Finish Styles

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,6 @@
+import { Dimensions, ScrollView, StyleSheet, Text, View } from "react-native";
 
-import { StyleSheet, Text, View } from "react-native";
+const {width: SCREEN_WIDTH} = Dimensions.get("window");
 
 export default function Index() {
   return (
@@ -7,12 +8,29 @@ export default function Index() {
       <View style={styles.city}>
         <Text style={styles.cityName}>Ho Chi Minh</Text>
       </View>
-      <View style={styles.weather}>
+      <ScrollView 
+        pagingEnabled 
+        horizontal  
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={styles.weather}
+      >
         <View style={styles.day}>
          <Text style={styles.temp}> 27 </Text>
          <Text style={styles.description}> Sunny </Text>
         </View>
-      </View>                                                                                     
+        <View style={styles.day}>
+         <Text style={styles.temp}> 27 </Text>
+         <Text style={styles.description}> Sunny </Text>
+        </View>
+        <View style={styles.day}>
+         <Text style={styles.temp}> 27 </Text>
+         <Text style={styles.description}> Sunny </Text>
+        </View>
+        <View style={styles.day}>
+         <Text style={styles.temp}> 27 </Text>
+         <Text style={styles.description}> Sunny </Text>
+        </View>
+      </ScrollView>                                                                                     
     </View>
   );
 }
@@ -33,10 +51,10 @@ export const styles = StyleSheet.create({
   }
   ,
   weather: {
-    flex: 3,
+
   },
   day: {
-    flex: 1,
+    width: SCREEN_WIDTH,
     alignItems: "center",
   },
   temp: {
