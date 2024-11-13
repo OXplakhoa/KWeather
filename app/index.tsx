f5c04585326bcd649bb00b55a013d8c94e0b0c42
OXplakhoa <OXplakhoa@gmail.com>
2024-11-13 13:22:55 +0700

    Fetch API & Display Weather

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,15 @@
 import { useEffect, useState } from "react";
-import { Dimensions, ScrollView, StyleSheet, Text, View } from "react-native";
+import { ActivityIndicator, Dimensions, ScrollView, StyleSheet, Text, View } from "react-native";
 import * as Location from 'expo-location';
 
 const {width: SCREEN_WIDTH} = Dimensions.get("window");
+const API_KEY = "8f63b0f21591bad5bd1a39eb583f51b2"
 
 export default function Index() {
   const [city,setCity] = useState("Loading...");
-  const [loction,setLocation] = useState(null);
+  const [days,setDays] = useState([])
   const [ok,setOk] = useState(true);
-  const ask = async () => {
+  const getWeather = async () => {
     const {granted} =  await Location.requestForegroundPermissionsAsync()
     if(!granted){
       setOk(false);
@@ -16,9 +17,27 @@ export default function Index() {
     const {coords: {latitude,longitude}}= await Location.getCurrentPositionAsync({accuracy: 5})
     const location = await Location.reverseGeocodeAsync({latitude,longitude},{useGoogleMaps: false})
     setCity(location[0].city ?? "Unknown");
+    try {
+      const res = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`);
+      if (!res.ok) {
+        console.error('API response error:', res.status);
+        return;
+      }
+      const json = await res.json();
+      // console.log(JSON.stringify(json, null, 2));
+      const daily = json.list.filter((weather: { dt_txt: string }) => {
+        if (weather.dt_txt.includes("00:00:00")) {
+          return weather;
+        }
+      });
+      setDays(daily);
+      console.log(JSON.stringify(daily,null,2));
+    } catch (error) {
+      console.error("Error fetching weather data:", error);
+    }
   }
   useEffect(() => {
-    ask()
+    getWeather()
   },[])
   return (
     <View style={styles.container}>
@@ -31,22 +50,18 @@ export default function Index() {
         showsHorizontalScrollIndicator={false}
         contentContainerStyle={styles.weather}
       >
-        <View style={styles.day}>
-         <Text style={styles.temp}> 27 </Text>
-         <Text style={styles.description}> Sunny </Text>
+        { days.length === 0 ? (
+          <View style={styles.day}>
+          <ActivityIndicator size="large" color="#0000ff"/>
         </View>
-        <View style={styles.day}>
-         <Text style={styles.temp}> 27 </Text>
-         <Text style={styles.description}> Sunny </Text>
-        </View>
-        <View style={styles.day}>
-         <Text style={styles.temp}> 27 </Text>
-         <Text style={styles.description}> Sunny </Text>
-        </View>
-        <View style={styles.day}>
-         <Text style={styles.temp}> 27 </Text>
-         <Text style={styles.description}> Sunny </Text>
-        </View>
+        ) : (
+          days.map((day:any,idx) => 
+          <View style={styles.day} key={idx}>
+            <Text style={styles.temp}>{parseFloat(day.main.temp).toFixed(1)}</Text>
+            <Text style={styles.summary}>{day.weather[0].main}</Text>
+            <Text style={styles.description}>{day.weather[0].description}</Text>
+          </View>)
+          )}
       </ScrollView>                                                                                     
     </View>
   );
@@ -78,8 +93,11 @@ export const styles = StyleSheet.create({
     marginTop: 50,
     fontSize: 178
   },
-  description: {
+  summary: {
     marginTop: -30,
     fontSize: 60,
+  },
+  description: {
+    fontSize: 20,
   }
 })
